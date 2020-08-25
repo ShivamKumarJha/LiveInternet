92fca4d1349ec583f2ac20d595768f9dd70f9ca1
ShivamKumarJha <jha.shivam3@gmail.com>
2020-08-25 14:41:35 +0530

    Update for minSdkVersion 16
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/liveinternet/viewmodel/ConnectionLiveData.kt
+++ app/src/main/java/com/shivamkumarjha/liveinternet/viewmodel/ConnectionLiveData.kt
@@ -2,52 +2,46 @@ package com.shivamkumarjha.liveinternet.viewmodel
 
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.os.Build
 import androidx.lifecycle.LiveData
 
-class ConnectionLiveData internal constructor(private val connectivityManager: ConnectivityManager) :
+class ConnectionLiveData(private val connectivityManager: ConnectivityManager) :
     LiveData<Boolean>() {
 
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-        override fun onAvailable(network: Network) {
-            postValue(true)
-        }
-
-        override fun onLost(network: Network) {
-            postValue(false)
-        }
-    }
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
 
     override fun onActive() {
         super.onActive()
+        postValue(false) // default no internet
+        if (Build.VERSION.SDK_INT >= 21) {
+            networkCallback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    postValue(true)
+                }
 
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ->
-                    postValue(true)
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ->
-                    postValue(true)
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ->
-                    postValue(true)
-                else -> postValue(false)
+                override fun onLost(network: Network) {
+                    postValue(false)
+                }
             }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo
+            val result = networkInfo != null && networkInfo.isConnected
+            postValue(result)
         }
-        postValue(false)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             connectivityManager.registerDefaultNetworkCallback(networkCallback)
-        } else {
+        } else if (Build.VERSION.SDK_INT >= 21) {
             val builder = NetworkRequest.Builder()
             connectivityManager.registerNetworkCallback(builder.build(), networkCallback)
         }
     }
 
     override fun onInactive() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
         super.onInactive()
-        connectivityManager.unregisterNetworkCallback(networkCallback)
     }
 }
